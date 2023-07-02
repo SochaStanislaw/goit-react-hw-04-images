af5d38ba8a574e4d271f3e8b38dc1e86ba748ccd
SochaStanislaw <socha.stanislaw95@gmail.com>
2023-07-02 18:44:36 +0200

    add hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 // api:
 import fetchImages from 'API/fetchImages';
 // components:
@@ -8,91 +9,155 @@ import Loader from './Loader';
 import Modal from './Modal';
 import Searchbar from './Searchbar';
 
-export default class App extends Component {
-  state = {
-    images: [],
-    page: 1,
-    query: '',
-    loading: false,
-    // error: null,
-    totalPages: 0,
-    // moreItems: true,
-    largeImageURL: '',
-  };
+const App = () => {
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
+  const [largeImageURL, setlargeImageURL] = useState('');
 
-  componentDidUpdate(prevProps, prevState) {
-    const prevQuery = prevState.query;
-    const nextQuery = this.state.query;
-
-    if (prevQuery !== nextQuery) {
-      this.getImages();
+  useEffect(() => {
+    if (query) {
+      getImages();
     }
-  }
+  }, [query]);
 
-  getImages = () => {
-    const { query, page } = this.state;
+  const getImages = () => {
+    setLoading(true);
 
-    this.setState({ loading: true });
-
-    fetchImages(query, page)
-      .then(fetchedImg => {
+    fetchImages(query, page).then(
+      fetchedImg => {
         if (fetchedImg.hits.length) {
-          this.setState(prevState => ({
-            images: [...prevState.images, ...fetchedImg.hits],
-            page: prevState.page + 1,
-            totalPages: Math.round(fetchedImg.totalHits / 12),
-          }));
-        } 
-      })
-      .catch(error => this.setState({ error }))
-      .finally(() => this.setState({ loading: false }));
+          setImages(prevImages => [...prevImages, ...fetchedImg.hits]);
+          setPage(prevPage => prevPage + 1);
+          setTotalPages(Math.round(fetchedImg.totalHits / 12));
+        }
+      }
+    )
+    .catch(error => console.log(error))
+    .finally(() => setLoading(false));
   };
 
-  getSearchFormSubmit = query => {
-    this.setState({
-      query: query,
-      page: 1,
-      images: [],
-      error: null,
-      moreItems: true,
-    });
+  const getSearchFormSubmit = query => {
+    setQuery(query);
+    setPage(1);
+    setImages([]);
   };
 
-  getLargeImage = url => {
-    this.setState({
-      largeImageURL: url,
-    });
+  const getLargeImage = url => {
+    setlargeImageURL(url);
   };
 
-  closeModal = () => {
-    this.setState({ largeImageURL: '' });
+  const closeModal = () => {
+    setlargeImageURL('');
   };
+  
+  return (
+    <div className='App'>
+      <Searchbar getSearchFormSubmit={getSearchFormSubmit} />
 
-  render() {
-    const {
-      images,
-      loading,
-      page,
-      totalPages,
-      largeImageURL,
-    } = this.state;
+      <ImageGallery images={images} getLargeImage={getLargeImage} />
+      
+      {loading && <Loader />}
+      
+      {!loading && totalPages >= page && (
+        <Button getImages={getImages} />
+      )}
+      
+      {largeImageURL && (
+        <Modal largeImageURL={largeImageURL} closeModal={closeModal} />
+      )}
+    </div>
+  );
+};
 
-    return (
-      <div className='App'>
-        <Searchbar getSearchFormSubmit={this.getSearchFormSubmit} />
+export default App;
+// export default class App extends Component {
+//   state = {
+//     images: [],
+//     page: 1,
+//     query: '',
+//     loading: false,
+//     // error: null,
+//     totalPages: 0,
+//     // moreItems: true,
+//     largeImageURL: '',
+//   };
 
-        <ImageGallery images={images} getLargeImage={this.getLargeImage} />
+//   componentDidUpdate(prevProps, prevState) {
+//     const prevQuery = prevState.query;
+//     const nextQuery = this.state.query;
+
+//     if (prevQuery !== nextQuery) {
+//       this.getImages();
+//     }
+//   }
+
+//   getImages = () => {
+//     const { query, page } = this.state;
+
+//     this.setState({ loading: true });
+
+//     fetchImages(query, page)
+//       .then(fetchedImg => {
+//         if (fetchedImg.hits.length) {
+//           this.setState(prevState => ({
+//             images: [...prevState.images, ...fetchedImg.hits],
+//             page: prevState.page + 1,
+//             totalPages: Math.round(fetchedImg.totalHits / 12),
+//           }));
+//         } 
+//       })
+//       .catch(error => this.setState({ error }))
+//       .finally(() => this.setState({ loading: false }));
+//   };
+
+//   getSearchFormSubmit = query => {
+//     this.setState({
+//       query: query,
+//       page: 1,
+//       images: [],
+//       error: null,
+//       moreItems: true,
+//     });
+//   };
+
+//   getLargeImage = url => {
+//     this.setState({
+//       largeImageURL: url,
+//     });
+//   };
+
+//   closeModal = () => {
+//     this.setState({ largeImageURL: '' });
+//   };
+
+//   render() {
+//     const {
+//       images,
+//       loading,
+//       page,
+//       totalPages,
+//       largeImageURL,
+//     } = this.state;
+
+//     return (
+//       <div className='App'>
+//         <Searchbar getSearchFormSubmit={this.getSearchFormSubmit} />
+
+//         <ImageGallery images={images} getLargeImage={this.getLargeImage} />
         
-        {loading && <Loader />}
+//         {loading && <Loader />}
         
-        {!loading && totalPages >= page && (
-          <Button getImages={this.getImages} />
-        )}
+//         {!loading && totalPages >= page && (
+//           <Button getImages={this.getImages} />
+//         )}
         
-        {largeImageURL && (
-          <Modal largeImageURL={largeImageURL} closeModal={this.closeModal} />
-        )}
-      </div>
-    );
-  }
-}
+//         {largeImageURL && (
+//           <Modal largeImageURL={largeImageURL} closeModal={this.closeModal} />
+//         )}
+//       </div>
+//     );
+//   }
+// }
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,33 +1,64 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-export default class Modal extends Component {
-  static propTypes = {
-    largeImageURL: PropTypes.string,
-    closeModal: PropTypes.func,
-  };
-
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleKeyDown = ev => {
+const Modal = ({ largeImageURL, closeModal }) => {
+  const handleKeyDown = (ev) => {
     if (ev.code === 'Escape') {
-      this.props.closeModal();
+      closeModal();
     }
   };
 
-  render() {
-    return (
-      <div className='Overlay' onClick={this.props.closeModal}>
-        <div className='Modal'>
-          <img src={this.props.largeImageURL} alt="" />
-        </div>
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
+  return (
+    <div className='Overlay' onClick={closeModal}>
+      <div className='Modal'>
+        <img src={largeImageURL} alt="" />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+Modal.propTypes = {
+  largeImageURL: PropTypes.string,
+  closeModal: PropTypes.func,
+};
+
+export default Modal;
+
+// export default class Modal extends Component {
+//   static propTypes = {
+//     largeImageURL: PropTypes.string,
+//     closeModal: PropTypes.func,
+//   };
+
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   handleKeyDown = ev => {
+//     if (ev.code === 'Escape') {
+//       this.props.closeModal();
+//     }
+//   };
+
+//   render() {
+//     return (
+//       <div className='Overlay' onClick={this.props.closeModal}>
+//         <div className='Modal'>
+//           <img src={this.props.largeImageURL} alt="" />
+//         </div>
+//       </div>
+//     );
+//   }
+// }
