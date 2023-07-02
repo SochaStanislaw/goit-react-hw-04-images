d176b80cc9f1d57bc9dc0a96f8a4c0b380572843
SochaStanislaw <socha.stanislaw95@gmail.com>
2023-07-02 19:19:32 +0200

    Update App.jsx
    add useCallback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
-// import React, { Component } from 'react';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 // api:
 import fetchImages from 'API/fetchImages';
 // components:
@@ -13,33 +12,32 @@ const App = () => {
   const [images, setImages] = useState([]);
   const [page, setPage] = useState(1);
   const [query, setQuery] = useState('');
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
   const [totalPages, setTotalPages] = useState(0);
   const [largeImageURL, setlargeImageURL] = useState('');
 
-  const getImages = () => {
+  const getImages = useCallback(() => {
     setLoading(true);
 
-    fetchImages(query, page).then(
-      fetchedImg => {
+    fetchImages(query, page)
+      .then((fetchedImg) => {
         if (fetchedImg.hits.length) {
-          setImages(prevImages => [...prevImages, ...fetchedImg.hits]);
-          setPage(prevPage => prevPage + 1);
+          setImages((prevImages) => [...prevImages, ...fetchedImg.hits]);
+          setPage((prevPage) => prevPage + 1);
           setTotalPages(Math.round(fetchedImg.totalHits / 12));
         }
-      }
-    )
-    .catch(error => console.log(error))
-    .finally(() => setLoading(false));
-  };
+      })
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }, [query, page]);
 
-  const getSearchFormSubmit = query => {
+  const getSearchFormSubmit = (query) => {
     setQuery(query);
     setPage(1);
     setImages([]);
   };
 
-  const getLargeImage = url => {
+  const getLargeImage = (url) => {
     setlargeImageURL(url);
   };
 
@@ -51,20 +49,18 @@ const App = () => {
     if (query) {
       getImages();
     }
-  }, [query]);
-  
+  }, [query, getImages]);
+
   return (
-    <div className='App'>
+    <div className="App">
       <Searchbar getSearchFormSubmit={getSearchFormSubmit} />
 
       <ImageGallery images={images} getLargeImage={getLargeImage} />
-      
+
       {loading && <Loader />}
-      
-      {!loading && totalPages >= page && (
-        <Button getImages={getImages} />
-      )}
-      
+
+      {!loading && totalPages >= page && <Button getImages={getImages} />}
+
       {largeImageURL && (
         <Modal largeImageURL={largeImageURL} closeModal={closeModal} />
       )}
@@ -73,91 +69,3 @@ const App = () => {
 };
 
 export default App;
-// export default class App extends Component {
-//   state = {
-//     images: [],
-//     page: 1,
-//     query: '',
-//     loading: false,
-//     // error: null,
-//     totalPages: 0,
-//     // moreItems: true,
-//     largeImageURL: '',
-//   };
-
-//   componentDidUpdate(prevProps, prevState) {
-//     const prevQuery = prevState.query;
-//     const nextQuery = this.state.query;
-
-//     if (prevQuery !== nextQuery) {
-//       this.getImages();
-//     }
-//   }
-
-//   getImages = () => {
-//     const { query, page } = this.state;
-
-//     this.setState({ loading: true });
-
-//     fetchImages(query, page)
-//       .then(fetchedImg => {
-//         if (fetchedImg.hits.length) {
-//           this.setState(prevState => ({
-//             images: [...prevState.images, ...fetchedImg.hits],
-//             page: prevState.page + 1,
-//             totalPages: Math.round(fetchedImg.totalHits / 12),
-//           }));
-//         } 
-//       })
-//       .catch(error => this.setState({ error }))
-//       .finally(() => this.setState({ loading: false }));
-//   };
-
-//   getSearchFormSubmit = query => {
-//     this.setState({
-//       query: query,
-//       page: 1,
-//       images: [],
-//       error: null,
-//       moreItems: true,
-//     });
-//   };
-
-//   getLargeImage = url => {
-//     this.setState({
-//       largeImageURL: url,
-//     });
-//   };
-
-//   closeModal = () => {
-//     this.setState({ largeImageURL: '' });
-//   };
-
-//   render() {
-//     const {
-//       images,
-//       loading,
-//       page,
-//       totalPages,
-//       largeImageURL,
-//     } = this.state;
-
-//     return (
-//       <div className='App'>
-//         <Searchbar getSearchFormSubmit={this.getSearchFormSubmit} />
-
-//         <ImageGallery images={images} getLargeImage={this.getLargeImage} />
-        
-//         {loading && <Loader />}
-        
-//         {!loading && totalPages >= page && (
-//           <Button getImages={this.getImages} />
-//         )}
-        
-//         {largeImageURL && (
-//           <Modal largeImageURL={largeImageURL} closeModal={this.closeModal} />
-//         )}
-//       </div>
-//     );
-//   }
-// }
